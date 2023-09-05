bf78e14a138e7b579c7f770ded81c8812e35a510
RaveYard <29225776+MrRaveYard@users.noreply.github.com>
2023-09-06 00:06:49 +0200

    Revert "Fix nullptr crashes by allowing weird subsectors and broken geometry"
    This reverts commit 54ac538b0dc6096288b1d1a282ed56cf56c67eab.

--- src/rendering/hwrenderer/doom_levelmesh.cpp
+++ src/rendering/hwrenderer/doom_levelmesh.cpp
@@ -52,10 +52,13 @@ DoomLevelMesh::DoomLevelMesh(FLevelLocals &doomMap)
 		{
 			for (int j = 2; j < numVerts; j++)
 			{
-				MeshElements.Push(pos);
-				MeshElements.Push(pos + j - 1);
-				MeshElements.Push(pos + j);
-				MeshSurfaceIndexes.Push((int)i);
+				if (!IsDegenerate(verts[0], verts[j - 1], verts[j]))
+				{
+					MeshElements.Push(pos);
+					MeshElements.Push(pos + j - 1);
+					MeshElements.Push(pos + j);
+					MeshSurfaceIndexes.Push((int)i);
+				}
 			}
 		}
 		else if (s.Type == ST_MIDDLESIDE || s.Type == ST_UPPERSIDE || s.Type == ST_LOWERSIDE)
@@ -690,6 +693,11 @@ void DoomLevelMesh::CreateSubsectorSurfaces(FLevelLocals &doomMap)
 	for (unsigned int i = 0; i < doomMap.subsectors.Size(); i++)
 	{
 		subsector_t *sub = &doomMap.subsectors[i];
+
+		if (sub->numlines < 3)
+		{
+			continue;
+		}
 
 		sector_t *sector = sub->sector;
 		if (!sector || IsControlSector(sector))
